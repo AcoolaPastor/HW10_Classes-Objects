6369078a604cc29acc1e7a42582280101a5b2972
Romano Acoola <romyok01@gmail.com>
2026-04-26 12:40:05 +0300

    Добавьте файлы проекта.

---
+++ Angle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10_Classes_Objects
+{
+    class Angle
+    {
+        int gradus, minute, second;
+        public bool isCorrect = true;
+        
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    isCorrect = false;
+                }
+                else
+                {
+                    gradus = value % 360;
+                }
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    isCorrect = false;
+                }
+                else 
+                {
+                    Gradus += value / 60;
+                    minute = value % 60;
+                }
+            }
+        }
+
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    isCorrect = false;
+                }
+                else
+                {
+                    Minute += value / 60;
+                    second = value % 60;
+                }
+            }
+        }
+
+        public Angle(int gradus, int minute, int second)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Second = second;
+        }
+        public double toRadian()
+        { 
+            return(double) (gradus + minute / 60.0 + second / 3600.0) * Math.PI / 180.0; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+//Реализовать класс, в котором указанные значения представлены в виде свойств.
+//Для свойств предусмотреть проверку корректности данных. Класс должен содержать конструктор для установки начальных
+//значений, а также метод ToRadians для перевода угла в радианы.
+//Создать объект на основе разработанного класса.
+//Осуществить использование объекта в программе.
+
+namespace HW10_Classes_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grad, min, sec;
+            Angle angle;
+
+            do
+            {
+                Console.WriteLine("Введите число в формате градус° минута' секунда\":");
+                grad = Convert.ToInt32(Console.ReadLine());
+                min = Convert.ToInt32(Console.ReadLine());
+                sec = Convert.ToInt32(Console.ReadLine());
+                angle = new Angle(grad, min, sec);
+            } while (angle.isCorrect == false);
+
+            Console.WriteLine($"Входное значение:\n{grad}° {min}' {sec}\"");
+            double rad = angle.toRadian();
+            //double radTested = (grad + min / 60.0 + sec / 3600.0) * Math.PI / 180.0;
+            Console.WriteLine($"Значение в радианах:\n{rad:F4}");
+
+        }
+    }
+}
